45fabf9bb663068bcb4db08e05feb50d1b1baeff
KishorNaik <kishor.naik011.net@gmail.com>
2020-03-11 19:24:25 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Schedulers/ScheduleTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AspNetCore.Scheduler.ScheduleTask;
+using System.IO;
+
+namespace Sol_Demo.Schedulers
+{
+    public class ScheduleTask : IScheduledTask
+    {
+        public string Schedule => "39 6 * * *";
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            string someText = "C# Corner is a community of software and data developers";
+            File.WriteAllText(@"C:\csc.txt", someText);
+
+            await Task.CompletedTask;
+        }
+    }
+}
